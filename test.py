c85cc5fce6f7bd4e40c13041b62f3c78c6ddb6ef
James Oswald <jamesoswald111@gmail.com>
2022-11-07 03:17:29 -0500

    Sampled from action mask

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import amrl
 import gymnasium as gym
 import random
+import numpy as np
 
 
 if __name__ == "__main__":
@@ -8,7 +9,7 @@ if __name__ == "__main__":
     env = gym.make("AMRL-MiniGrid-Unlock-2by2-v0", render_mode="human")
     observation = env.reset()
     for _ in range(1000):
-        action = env.action_space.sample()
+        action = env.action_space.sample(np.array(env.action_mask(), dtype="int8"))
         observation, reward, terminated, truncated, info = env.step(action)
         if terminated or truncated:
             observation, info = env.reset()
